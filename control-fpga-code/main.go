911e40e363805f8288b5ec69f760369f991c8e94
Jeff DeCola (TK2-PC) <jeffdecola@gmail.com>
2023-03-10 14:47:11 -0500

    update build push

--- control-fpga-code/main.go
+++ control-fpga-code/main.go
@@ -13,11 +13,10 @@ package main
 
 import (
 	"fmt"
-	"os"
-
-	"periph.io/x/conn/v3/gpio"
+	"time"
 )
 
+/*
 // OUTPUT PINS
 var pin_out_go gpio.PinIO
 var pin_out_led gpio.PinIO
@@ -25,81 +24,90 @@ var pin_out_opcode [4]gpio.PinIO
 
 // INPUT PINS
 var pin_in_data_out [8]gpio.PinIO
+*/
+
+var count int
 
 func main() {
-
-	fmt.Println("hi")
-	os.Exit(0)
-
-	/* var setGoLevelCh = make(chan int)
-	var userInput int
-	var getDataOutLevelCh = make(chan int)
-	var capturedDataOutLevelCh = make(chan [8]int)
-
-	// OUTPUTS -------------------------------------
-
-	// Set go
-	go set_go(setGoLevelCh)
-
-	// Set [3:0] opcode
-	err := pin_out_opcode[3].Out(gpio.High)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = pin_out_opcode[2].Out(gpio.High)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = pin_out_opcode[1].Out(gpio.Low)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = pin_out_opcode[0].Out(gpio.Low)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// INPUTS --------------------------------------
-
-	// Get level for [7:0] DATA_OUT
-	go get_data_out(getDataOutLevelCh, capturedDataOutLevelCh)
-
-	// CONTROL -------------------------------------
-
-	// USER INPUT
-	fmt.Println("Enter one of the following:")
-	fmt.Println("    0 - set go and led level LOW")
-	fmt.Println("    1 - set go and led level HIGH")
-	fmt.Println("    2 - get [7:0] DATA_OUT levels")
-	fmt.Println("    3 - Exit")
-
 	for {
 
-		fmt.Scanln(&userInput)
+		fmt.Println("Hello everyone, the count is:", count)
+		time.Sleep(2000 * time.Millisecond)
+		count++
 
-		switch {
-		case (userInput == 0):
-			fmt.Println("You pressed 0 - set go and led level LOW")
-			setGoLevelCh <- 0
-		case (userInput == 1):
-			fmt.Println("You pressed 1 - set go and led level HIGH")
-			setGoLevelCh <- 1
-		case (userInput == 2):
-			fmt.Println("You pressed 2 -  get [7:0] DATA_OUT levels")
-			getDataOutLevelCh <- 1
-			capture_data_out_level := <-capturedDataOutLevelCh
-			fmt.Printf("[7:0] DATA_OUT levels are %v  %v  %v  %v  %v  %v  %v  %v\n", capture_data_out_level[7], capture_data_out_level[6], capture_data_out_level[5],
-				capture_data_out_level[4], capture_data_out_level[3], capture_data_out_level[2], capture_data_out_level[1], capture_data_out_level[0])
-		case (userInput == 3):
-			fmt.Println("You pressed 3 - Exit")
-		default:
-			fmt.Println("Try Again")
-		}
+	}
 
-		if userInput == 3 {
-			break
-		}
+	/*
+		  var setGoLevelCh = make(chan int)
+			var userInput int
+			var getDataOutLevelCh = make(chan int)
+			var capturedDataOutLevelCh = make(chan [8]int)
 
-	} */
+			// OUTPUTS -------------------------------------
+
+			// Set go
+			go set_go(setGoLevelCh)
+
+			// Set [3:0] opcode
+			err := pin_out_opcode[3].Out(gpio.High)
+			if err != nil {
+				log.Fatal(err)
+			}
+			err = pin_out_opcode[2].Out(gpio.High)
+			if err != nil {
+				log.Fatal(err)
+			}
+			err = pin_out_opcode[1].Out(gpio.Low)
+			if err != nil {
+				log.Fatal(err)
+			}
+			err = pin_out_opcode[0].Out(gpio.Low)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// INPUTS --------------------------------------
+
+			// Get level for [7:0] DATA_OUT
+			go get_data_out(getDataOutLevelCh, capturedDataOutLevelCh)
+
+			// CONTROL -------------------------------------
+
+			// USER INPUT
+			fmt.Println("Enter one of the following:")
+			fmt.Println("    0 - set go and led level LOW")
+			fmt.Println("    1 - set go and led level HIGH")
+			fmt.Println("    2 - get [7:0] DATA_OUT levels")
+			fmt.Println("    3 - Exit")
+
+			for {
+
+				fmt.Scanln(&userInput)
+
+				switch {
+				case (userInput == 0):
+					fmt.Println("You pressed 0 - set go and led level LOW")
+					setGoLevelCh <- 0
+				case (userInput == 1):
+					fmt.Println("You pressed 1 - set go and led level HIGH")
+					setGoLevelCh <- 1
+				case (userInput == 2):
+					fmt.Println("You pressed 2 -  get [7:0] DATA_OUT levels")
+					getDataOutLevelCh <- 1
+					capture_data_out_level := <-capturedDataOutLevelCh
+					fmt.Printf("[7:0] DATA_OUT levels are %v  %v  %v  %v  %v  %v  %v  %v\n", capture_data_out_level[7], capture_data_out_level[6], capture_data_out_level[5],
+						capture_data_out_level[4], capture_data_out_level[3], capture_data_out_level[2], capture_data_out_level[1], capture_data_out_level[0])
+				case (userInput == 3):
+					fmt.Println("You pressed 3 - Exit")
+				default:
+					fmt.Println("Try Again")
+				}
+
+				if userInput == 3 {
+					break
+				}
+
+			}
+	*/
 
 }
--- control-fpga-code/outputs.go
+++ control-fpga-code/outputs.go
@@ -1,49 +1,45 @@
 package main
 
-import (
-	"fmt"
-
-	"periph.io/x/conn/v3/gpio"
-)
-
+/*
 // DATA_OUT (GET)
 func get_data_out(getDataOutLevelCh chan int, capturedDataOutLevelCh chan [8]int) {
 
-	var captured_data_out_level [8]gpio.Level
-	var captured_data_out_level_binary [8]int
+		var captured_data_out_level [8]gpio.Level
+		var captured_data_out_level_binary [8]int
 
-	// Loop forever
-	for {
+		// Loop forever
+		for {
 
-		fmt.Println("get_data_out() - Waiting to capture data_out...")
-		get_data_out_level := <-getDataOutLevelCh
+			fmt.Println("get_data_out() - Waiting to capture data_out...")
+			get_data_out_level := <-getDataOutLevelCh
 
-		fmt.Printf("get_data_out() - Getting data_out Level, %v \n", get_data_out_level)
+			fmt.Printf("get_data_out() - Getting data_out Level, %v \n", get_data_out_level)
 
-		// GET THE LEVEL
-		captured_data_out_level[7] = pin_in_data_out[7].Read()
-		captured_data_out_level[6] = pin_in_data_out[6].Read()
-		captured_data_out_level[5] = pin_in_data_out[5].Read()
-		captured_data_out_level[4] = pin_in_data_out[4].Read()
-		captured_data_out_level[3] = pin_in_data_out[3].Read()
-		captured_data_out_level[2] = pin_in_data_out[2].Read()
-		captured_data_out_level[1] = pin_in_data_out[1].Read()
-		captured_data_out_level[0] = pin_in_data_out[0].Read()
+			// GET THE LEVEL
+			captured_data_out_level[7] = pin_in_data_out[7].Read()
+			captured_data_out_level[6] = pin_in_data_out[6].Read()
+			captured_data_out_level[5] = pin_in_data_out[5].Read()
+			captured_data_out_level[4] = pin_in_data_out[4].Read()
+			captured_data_out_level[3] = pin_in_data_out[3].Read()
+			captured_data_out_level[2] = pin_in_data_out[2].Read()
+			captured_data_out_level[1] = pin_in_data_out[1].Read()
+			captured_data_out_level[0] = pin_in_data_out[0].Read()
 
-		fmt.Printf("get_data_out() - Captured data_out level is %v \n", captured_data_out_level)
+			fmt.Printf("get_data_out() - Captured data_out level is %v \n", captured_data_out_level)
 
-		// Change High/Low to 1/0 respectively
-		for i := 0; i < 8; i++ {
-			if captured_data_out_level[i] == gpio.Low {
-				captured_data_out_level_binary[i] = 0
-			} else {
-				captured_data_out_level_binary[i] = 1
+			// Change High/Low to 1/0 respectively
+			for i := 0; i < 8; i++ {
+				if captured_data_out_level[i] == gpio.Low {
+					captured_data_out_level_binary[i] = 0
+				} else {
+					captured_data_out_level_binary[i] = 1
 
+				}
 			}
+
+			capturedDataOutLevelCh <- captured_data_out_level_binary
+
 		}
 
-		capturedDataOutLevelCh <- captured_data_out_level_binary
-
-	}
-
 }
+*/
