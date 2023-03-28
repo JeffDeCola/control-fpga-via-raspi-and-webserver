efe8b44a1a07b3d28d51032d6d5976af0830fe70
Jeff DeCola (TK2-PC) <jeffdecola@gmail.com>
2023-03-28 05:38:58 -0400

    added a ci concourse docker deploy resource

--- ci/scripts/deploy.sh
+++ ci/scripts/deploy.sh
@@ -37,52 +37,5 @@ echo "List whats in the current directory"
 ls -la
 echo " "
 
-echo "DOCKER RUN ----------------------------------------------------------------------------"
-echo " "
-
-echo "DOCKER HOST INFO:"
-# echo "$DOCKER_HOST_SSH_PRIVATE_KEY_FILE"
-echo "$DOCKER_HOST_PORT"
-echo "$DOCKER_HOST_USER"
-echo "$DOCKER_HOST_IP"
-echo " "
-
-echo "Put private key in temp file"
-echo "$DOCKER_HOST_SSH_PRIVATE_KEY_FILE" >> private-key-file.txt
-cat private-key-file.txt
-chmod 600 private-key-file.txt
-echo " "
-
-echo "Stop old container if it exists - Ignore if you get an error"
-echo "docker stop control-fpga-via-raspi-and-webserver || true "
-ssh -o StrictHostKeyChecking=no \
-    -i private-key-file.txt -p "$DOCKER_HOST_PORT" "$DOCKER_HOST_USER"@"$DOCKER_HOST_IP" \
-    'docker stop control-fpga-via-raspi-and-webserver || true'
-echo " "
-
-echo "Remove old container if it exsits - Ignore if you get an error"
-echo "docker rm control-fpga-via-raspi-and-webserver || true"
-ssh -o StrictHostKeyChecking=no \
-    -i private-key-file.txt -p "$DOCKER_HOST_PORT" "$DOCKER_HOST_USER"@"$DOCKER_HOST_IP" \
-    'docker rm control-fpga-via-raspi-and-webserver || true'
-echo " "
-
-echo "docker run --name control-fpga-via-raspi-and-webserver -dit jeffdecola/control-fpga-via-raspi-and-webserver"
-ssh -o StrictHostKeyChecking=no \
-    -i private-key-file.txt -p "$DOCKER_HOST_PORT" "$DOCKER_HOST_USER"@"$DOCKER_HOST_IP" \
-    'docker run --name control-fpga-via-raspi-and-webserver -dit jeffdecola/control-fpga-via-raspi-and-webserver'
-echo " "
-
-echo "rm private-key-file.txt"
-rm private-key-file.txt
-echo " "
-
-echo "You can now run docker commands such as:"
-echo "docker run --name control-fpga-via-raspi-and-webserver -dit jeffdecola/control-fpga-via-raspi-and-webserver"
-echo "docker exec -i -t control-fpga-via-raspi-and-webserver /bin/bash"
-echo "docker logs control-fpga-via-raspi-and-webserver"
-echo "docker rm -f control-fpga-via-raspi-and-webserver"
-echo " "
-
 echo "deploy.sh (END)"
 echo " "
